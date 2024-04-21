bf4542c792d57ca7c6b301f6cbeb4246124818c9
Vinícius <viniscrv@gmail.com>
2024-04-21 11:00:30 -0300

    feat: add navbar

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,5 +1,29 @@
+import { ChartBar } from "@phosphor-icons/react";
+import { NavLink } from "react-router-dom";
+
 export function Home() {
+    const pages = ["dashboard-01", "dashboard-02", "dashboard-03"]
+
     return (
-        <h1>Home</h1>
+        <div className="flex">
+            <nav className="w-44 min-h-screen bg-neutral-900">
+                <h2 className="w-full text-center font-bold text-xl py-8">Nome bom</h2>
+
+                <ul className="flex flex-col gap-2">
+                    {pages.map((page) => {
+                        return (
+                            <li key={page}>
+                                <NavLink to={"/home"} className={"flex items-center justify-center gap-2"}>
+                                    <ChartBar size={18} color="#8B8B8B"></ChartBar>
+                                    <span className="text-neutral-400">{page}</span>
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                    
+                </ul>
+            </nav>
+            <header>header</header>
+        </div>
     )
 }
