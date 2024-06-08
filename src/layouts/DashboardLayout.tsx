8dccf5ac0300930bd1ef049df2f1d5362d4e1bb9
Vinícius <viniscrv@gmail.com>
2024-06-07 21:39:38 -0300

    feat: visual response to navbar status

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -12,27 +12,27 @@ export function DashboardLayout() {
     const pages = [
         {
             name: "Meu Perfil",
-            icon: <User size={18} color="#8B8B8B" />,
+            icon: <User size={18} color="#8B8B8B" className="group-hover:fill-neutral-100 group-[&.active]:fill-neutral-100" />,
             path: "/dashboard/me"
         },
         {
             name: "Hoje",
-            icon: <CalendarCheck size={18} color="#8B8B8B" />,
+            icon: <CalendarCheck size={18} color="#8B8B8B" className="group-hover:fill-neutral-100 group-[&.active]:fill-neutral-100" />,
             path: "/dashboard/today"
         },
         {
             name: "Histórico",
-            icon: <ClockCounterClockwise size={18} color="#8B8B8B" />,
+            icon: <ClockCounterClockwise size={18} color="#8B8B8B" className="group-hover:fill-neutral-100 group-[&.active]:fill-neutral-100" />,
             path: "/dashboard/history"
         },
         {
             name: "Atividades",
-            icon: <ListBullets size={18} color="#8B8B8B" />,
+            icon: <ListBullets size={18} color="#8B8B8B" className="group-hover:fill-neutral-100 group-[&.active]:fill-neutral-100" />,
             path: "/dashboard/activity-manager"
         },
         {
             name: "Estatísticas",
-            icon: <ChartLineUp size={18} color="#8B8B8B" />,
+            icon: <ChartLineUp size={18} color="#8B8B8B" className="group-hover:fill-neutral-100 group-[&.active]:fill-neutral-100"/>,
             path: "/dashboard/statistics"
         }
     ];
@@ -70,11 +70,11 @@ export function DashboardLayout() {
                                     <NavLink
                                         to={page.path}
                                         className={
-                                            "flex items-center justify-center gap-2 pl-6"
+                                            "flex items-center justify-center gap-2 pl-6 group"
                                         }
                                     >
                                         {page.icon}
-                                        <span className="text-neutral-400">
+                                        <span className="text-neutral-400 group-hover:text-neutral-100 group-[&.active]:text-neutral-100">
                                             {page.name}
                                         </span>
                                     </NavLink>
