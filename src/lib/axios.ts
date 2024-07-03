7bf937017249e8727c7ac74e31ee711e381b7615
Vinícius <viniscrv@gmail.com>
2024-07-03 12:07:10 -0300

    feat: add api interceptor to logout user when token expire

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,3 +3,16 @@ import axios from "axios"
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 })
+
+api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (localStorage.getItem("token") && error.response && error.response.status === 401) {
+        localStorage.removeItem("token");
+        window.location.href = "/";
+      }
+      return Promise.reject(error);
+    }
+  );
